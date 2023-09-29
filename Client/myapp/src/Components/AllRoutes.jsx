5f3f2bc284cf84edfaa8f8a2795325374838d5e9
smartcoin1207 <jackle.some99@gmail.com>
2023-09-29 14:45:09 +0530

    product aded

--- Client/myapp/src/Components/AllRoutes.jsx
+++ Client/myapp/src/Components/AllRoutes.jsx
@@ -1,26 +1,29 @@
 import React from "react";
-import {Routes,Route} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
-import Cart from '../Pages/Cart/Cart';
+import Cart from "../Pages/Cart/Cart";
 import Checkout from "../Pages/Checkoutpage";
 import Shopping from "../Pages/Shopping";
 import MensProductsCard from "../Pages/SingleProduct";
-import SignUp from "../Pages/AdminPanel/LoginSignUp/SignUp";
-import LoginForm from "../Pages/AdminPanel/LoginSignUp/Login";
-const AllRoutes  =()=>{
-    return(
-        <div>
-            <Routes>
-                <Route path="/" element={<Home/>}></Route>
-                <Route path="/cart" element={<Cart/>}></Route>
-                <Route path="/product/:id" element={<MensProductsCard/>}></Route>
-                <Route path="/shopping" element={<Shopping/>}> </Route>
-                <Route path="/checkout" element={<Checkout/>}></Route>
-                <Route path="/admin/signup" element={<SignUp/>}></Route>
-                <Route path="/admin/login" element={<LoginForm/>}></Route>
-            </Routes>
-        </div>
-    )
-}
+import SignUp from "../Pages/AdminPanel/LoginSignup/SignUp";
+import LoginForm from "../Pages/AdminPanel/LoginSignup/Login";
+
+const AllRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/product/:id" element={<MensProductsCard />}></Route>
+        <Route path="/shopping" element={<Shopping />}>
+          {" "}
+        </Route>
+        <Route path="/checkout" element={<Checkout />}></Route>
+        <Route path="/admin/signup" element={<SignUp />}></Route>
+        <Route path="/admin/login" element={<LoginForm />}></Route>
+      </Routes>
+    </div>
+  );
+};
 
 export default AllRoutes;
